2698bc0a560a0d5aa5bdd452c3df4160cb8f2d04
John Klaus <johnkariukiklaus@gmail.com>
2019-10-15 18:42:04 +0300

    python day one

---
+++ Python basics/Hello_World.py
@@ -0,0 +1,8 @@
+# the line below will print "hello world" on the console
+print("Hello World")
+ # the line below calculates
+ # the total of three numbers and stores
+ # the result to variable total.
+total = 40 + 50+ 32
+print(total)
+
---
+++ Python basics/Variables.py
@@ -0,0 +1,2 @@
+#variables are containers/ placeholders for storing data values for reference in memory
+#this memory is normally primary memory(RAM)
